b526993ccf3118b3d9bf8edf8f507071647ff32e
Rahat Chowdhury <rczisan@gmail.com>
2023-06-19 13:16:53 +0600

    added comments where necessary

--- OrangeHRM/Main_Method.py
+++ OrangeHRM/Main_Method.py
@@ -1,5 +1,4 @@
-
-
+# importing necessary packages and modules
 from Necessary_Packages import *
 from Name_Handling import handle_names
 from Login_Handling import handle_login_info
@@ -21,33 +20,76 @@ from Custom_Field_Save_Button_Handling import handle_custom_field_save_button
 def login_and_save_my_info():
     # checking if the title of the current url matches the homepage url
     try:
+        write_in_file("report.txt", "\n----Data starts here----\n")
         driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
         assert "https://opensource-demo.orangehrmlive.com" in driver.current_url, f"URL " \
                                                                                   f"mismatched!"
 
-        write_in_file("report.txt", "----Data starts here----")
         write_in_file("report.txt", "Homepage opened successfully!")
-
+        """
+        user login is checked here with the username and password information
+        """
         handle_login_info("Admin", "admin123")
+        """
+        my info button click is checked here
+        """
         handle_my_info()
+        """
+        usernames (firstname, middlename and lastname) fields are checked here
+        """
         handle_names()
+        """
+        employee id and other id fields are checked here
+        """
         handle_emp_id()
+        """
+        driving license field and license expiry date field are checked here
+        """
         handle_driving_license()
+        """
+        ssn and sin number fields are checked here
+        """
         handle_ssn_and_sin_number()
+        """
+        nationality (dropdown) field is checked here
+        """
         handle_nationality()
+        """
+        marital status (dropdown) field is checked here
+        """
         handle_marital_status()
+        """
+        Date of birth field is checked here
+        """
         handle_date_of_birth()
+        """
+        Gender field is checked here
+        """
         handle_gender()
+        """
+        Military service field is checked here
+        """
         handle_military_service_field()
+        """
+        Smoker Checker field is checked here
+        """
         handle_smoker_checker()
-
+        """
+        Save button click is checked here
+        """
         handle_save_button()
 
+        """
+        Custom field : Blood Type (dropdown) field is checked here
+        """
         handle_blood_type()
+        """
+        Custom field save button click is checked here
+        """
         handle_custom_field_save_button()
 
         time.sleep(3)
-        write_in_file("report.txt", "-----Data ends here-----")
+        write_in_file("report.txt", "\n-----Data ends here-----\n")
 
     except Exception as e:
         write_in_file("report.txt", "URL Exception:  " + type(e).__name__)
--- OrangeHRM/Smoker_Checker_Handling.py
+++ OrangeHRM/Smoker_Checker_Handling.py
@@ -6,10 +6,17 @@ def handle_smoker_checker():
         smoker_field = WebDriverWait(driver, 5, poll_frequency=1). \
             until(EC.visibility_of_element_located
                   ((By.CSS_SELECTOR,
+                    ".orangehrm-edit-employee-content .orangehrm-vertical-padding:nth-of-type(1) .oxd-checkbox-wrapper")))
+        smoker_field_label = WebDriverWait(driver, 5, poll_frequency=1). \
+            until(EC.visibility_of_element_located
+                  ((By.CSS_SELECTOR,
                     ".orangehrm-edit-employee-content .oxd-form-row:nth-child(7) .oxd-grid-item--gutters:nth-of-type(2) div:nth-of-type(2) label")))
 
-        smoker_field.click()
-        write_in_file("report.txt", "Smoker: " + smoker_field.text + " selected!")
+        if smoker_field.is_displayed():
+            smoker_field_label.click()
+            write_in_file("report.txt", "Smoker field: " + smoker_field.text + " selected!")
+        else:
+            write_in_file("report.txt", "Smoker: " + smoker_field.text + " selected!")
 
     except Exception as e:
         write_in_file("report.txt", "Smoker field exception: " + type(e).__name__)
